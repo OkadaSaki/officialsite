f555fdefd9535eacb0437736efea61e071ac0b08
DESKTOP-1A66N10\nuko- <osknteisyoku.umum@gmail.com>
2023-04-03 12:11:43 +0900

    今日分の作成まで

---
+++ js/sample.js
@@ -0,0 +1,23 @@
+//function header() {
+//  $.ajax({
+//    url: "header.html",
+//    cache: false,
+//    success: function (html) {
+//      document.write(html);
+//    }
+//  });
+//}
+$(function () {
+  $('.menu-trigger').click(function () { //ハンバーガーボタン(.menu-trigger)をクリック
+    $(this).toggleClass('active'); //ハンバーガーボタンに(.active)を追加・削除
+    if ($(this).hasClass('active')) { //もしハンバーガーボタンに(.active)があれば
+      $('.g-navi').addClass('active'); //(.g-navi)にも(.active)を追加
+    } else { //それ以外の場合は、
+      $('.g-navi').removeClass('active'); //(.g-navi)にある(.active)を削除
+    }
+  });
+  $('.nav-list ul li a').click(function () { //各メニュー(.nav-wrapper ul li a)をタップする
+    $('.menu-trigger').removeClass('active'); //ハンバーガーボタンにある(.active)を削除
+    $('.g-navi').removeClass('active'); //(.g-navi)にある(.active)も削除
+  });
+});
---
+++ report.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:700,400">
+  <title>食レポシリーズ</title>
+  <link rel="stylesheet" href="css/reset.css" type="text/css">
+  <link rel="stylesheet" href="css/style-l.css" media="screen and (min-width:769px)">
+  <link rel="stylesheet" href="css/style-s.css" media="screen and (max-width:768px)">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="js/script.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</head>
+
+
+<body>
+  <h1>食レポシリーズ表示確認</h1>
+
+  <?php
+    // 多階層からなるディレクトリ内のファイル一覧を取得する関数
+    function scandir_r($dir){
+        // scandirで指定されたパスだけのディレクトリとファイルを取得
+        $list = scandir($dir);
+
+        // ファイル一覧結果を詰めるための配列を定義(この時点では空)
+        $results = array();
+
+        // 取得したlistを検査
+        foreach($list as $record){
+            // scandirの仕様で不要な.が入ってしまうためそれらはファイル一覧結果に含めない
+            if(in_array($record, array(".", ".."))){
+                continue;
+            }
+            // scandirで取得した値はパスの最後若しくはファイル名のみなので、結合してパスとして補完する
+            $path = rtrim($dir, "/")."/".$record;
+            // 補完したパスがファイルへのパスだった場合、ファイル一覧結果に値を詰める
+            if(is_file($path)){
+                $results[] = $path;
+            }
+            else
+            {
+                // 補完したパスがディレクトリ(子階層)だった場合、自分自身を呼び出してまた上から処理を実行して最後のファイルまで検索していく
+                if(is_dir($path)){
+                    $results = array_merge($results, scandir_r($path));
+                }
+            }
+        }
+        return $results;
+    }
+
+    // 関数を実行してファイル一覧取得
+    // ここで指定するパスは実行するphpファイルからみたパス
+    $tmp = scandir_r("./images/report");
+    // 画像一覧の表示
+    // 店舗名は1度のみ表示するようにチェック処理を入れている
+    $shopName = "";
+    foreach($tmp as $v) {
+        if(basename(dirname($v)) !== $shopName){
+            // 店舗名表示
+            echo '<H3>' , basename(dirname($v)) , '</H3>';
+            $shopName = basename(dirname($v));
+        }
+        // 画像表示
+        echo '<img src="' , $v , '" alt="" loading="lazy" class="food">';
+        // ファイル名表示
+        echo '<label>' , basename($v) , '</label>';
+    }
+  ?>
+</body>
+</html>
